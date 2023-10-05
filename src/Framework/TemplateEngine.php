a1d328a84968105f90d4ce0152c74e076a38f634
AurinYang <aurin.coding@gmail.com>
2023-10-05 12:38:24 +1100

    template engine + home page

--- src/Framework/TemplateEngine.php
+++ src/Framework/TemplateEngine.php
@@ -6,4 +6,27 @@ namespace Framework;
 
 class TemplateEngine
 {
+    public function __construct(private string $basePath)
+    {
+    }
+
+    public function render(string $template, array $data = [])
+    {
+        extract($data, EXTR_SKIP);
+
+        ob_start();
+
+        include $this->resolve($template);
+
+        $output = ob_get_contents();
+
+        ob_end_clean();
+
+        return $output;
+    }
+
+    public function resolve(string $path)
+    {
+        return "{$this->basePath}/{$path}";
+    }
 }
